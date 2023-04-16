4808ffb51a29874e4b8ff8bde93d70908a0813aa
jeanniechamp <jeanniechamp@gmail.com>
2023-04-16 13:21:51 -0700

    variables_learn_1

--- variable.py
+++ variable.py
@@ -1,4 +1,16 @@
 temp = 10   #integer variable
 print(temp)  
 name = "Anna" #string variable
-print(name) #print  
+print(name) 
+marks = 19.3
+print(marks) #float variable
+result = True
+power = False
+print(result) #boolean variable eg. 1
+print(power) #boolean variable eg. 2
+fruits = ["apple, peach, banana"]
+colours = ["purple, blue, green"]
+print(fruits) #list variables eg.1
+print(colours) #list variables eg.2
+role_number = {25:"Jeannie", 24:"Luca"}
+print(role_number)
