62381f84851cb904eb159cc2d86c82801a3ae477
jeanniechamp <jeanniechamp@gmail.com>
2023-07-17 14:40:04 +0530

    kidsclub works yayy

--- kidsclub.py
+++ kidsclub.py
@@ -1,8 +1,6 @@
-##("You are eligible to join the kids club!)
-##("Sorry, you must be atleast 12 years old to join the kids club.")
 age = input("What is your age? ")
 age =int(age)
-if age <= 12 :
+if age >= 12 :
     print("You are eligible to join the kids club!")
-else:
+elif age < 12 : 
     print("Sorry, you must be atleast 12 years old to join the kids club.")
