629bbed4f29d0e54f543af6afef8762e657e07a9
jeanniechamp <jeanniechamp@gmail.com>
2023-07-25 19:48:14 +0530

    YAYY

---
+++ fruitss.py
@@ -0,0 +1,7 @@
+farut =input("What's your favorite fruit? ")
+farut=str(farut)
+if farut == ("apple"):
+    print("Good choice! Apples are delicious!")
+else:
+    print("That's a tasty fruit too!")
+    #IT WORKS YAYYYY
