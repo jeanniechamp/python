86bf1185f17d07d4b8b327a9e5065ef4329c548f
jeanniechamp <jeanniechamp@gmail.com>
2023-07-25 22:11:10 +0530

    DONEE

---
+++ bluefav.py
@@ -0,0 +1,6 @@
+bluecolour =input("What is your favourite colour? ")
+bluecolour=str(bluecolour)
+if bluecolour == ("blue"):
+    print("Great choice!")
+else:
+    print("That's a nice colour too!")
