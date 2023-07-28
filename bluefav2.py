ac1c88425da91b37bfe853fce2c0150971828ad8
jeanniechamp <jeanniechamp@gmail.com>
2023-07-28 12:11:01 +0530

    modified

---
+++ bluefav2.py
@@ -0,0 +1,10 @@
+ecolour =input("What is your favourite colour? ")
+ecolour=str(ecolour)
+if ecolour == ("blue"):
+    print("Great choice!")
+elif ecolour == ("pink"):
+    print("Good choice!")  
+elif ecolour == ("pink blue"):
+    print("Amazing choice!")
+else:
+    print("I don't know this colour.")
