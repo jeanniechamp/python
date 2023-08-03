33fddfc17af41b1e6f45a6b29b996ad99463735a
jeanniechamp <jeanniechamp@gmail.com>
2023-08-03 17:18:33 +0530

    im confused :(

--- nAme.py
+++ nAme.py
@@ -1,4 +1,4 @@
 namei = input("Enter your name:")
 x=("a") or ("A")
-if namei ("")
+if namei (""):
 #this code won't work because i dont know how to solve the question, i dont think i've learned anything on how to find the beggining letter of the input
