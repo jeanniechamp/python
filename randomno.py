67fa7d9fc7377aad96164b448e132f4b65b80900
jeanniechamp <jeanniechamp@gmail.com>
2023-08-26 17:05:52 +0530

    dfgblsdfk

--- randomno.py
+++ randomno.py
@@ -1,7 +1,9 @@
 import random
 
-num1 = random.randint(0, 9)
-print(num1)
-
-num2 = random.randint(10, 100)
-print(num2)
+num1 = random.randint(1, 3)
+guess = input("Guess the secret number between 1- 3: ")
+guess =int(guess)
+if guess == num1 :
+    print("Congrats!",num1,"was the right answer")
+else :
+    print("Wrong answer, the correct answer was",num1)
