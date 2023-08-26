63c4ed4e1e387389ad20ff5d5151da7b8c31bc2e
jeanniechamp <jeanniechamp@gmail.com>
2023-08-26 17:06:35 +0530

    hihihihi

---
+++ colourloop.py
@@ -0,0 +1,5 @@
+colours = ["blue", "purple", "red"]
+for x in colours:
+    if x == "purple":
+        continue
+    print(x)
---
+++ loop1.py
@@ -0,0 +1,6 @@
+print ("Start")
+for x in range(1,3,1):
+    y =input ("What is your name? ")
+    print (y)
+    print (x)
+print ("The End.")
