22517ea2134acd9b192df605886c36b27de55726
jeanniechamp <jeanniechamp@gmail.com>
2023-09-18 15:49:32 +0530

    YUNJIN!

---
+++ exercise/1comp_num.py
@@ -0,0 +1,6 @@
+x = input("Welcome to the jeannie calculator! Enter a number: ")
+y = input("Enter a second number to compare with: ")
+if x>y:
+    print(x, "is greater than", y)
+elif x<y:
+    print(y, "is greater than", x)
