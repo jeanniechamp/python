06b6b3bf38eb931c2437691816ac88d8126087c5
jeanniechamp <jeanniechamp@gmail.com>
2023-10-30 21:45:07 +0530

    help

---
+++ exercise/4leapyear_check.py
@@ -0,0 +1,8 @@
+#Exercise: Write a program to check whether an years is leap year or not.
+ly = input("Enter a year to find if its a leap year: ")
+ly =int(ly)
+print(ly%4)
+if ly == 0:
+    print("this year is a leap year!")
+else:
+    print("not a leap year!")
---
+++ exercise/5month.py
@@ -0,0 +1,1 @@
+#Exercise: Write a program to accept a number from 1 to 12 and display name of the month and days inthat month like 1 for January and number of days 31 and so on
