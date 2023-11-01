9b7778abee3deddbd12818d347e238f2e476e01c
jeanniechamp <jeanniechamp@gmail.com>
2023-11-01 23:19:52 +0530

    day 2

---
+++ __pycache__/addition_subtraction.py
@@ -0,0 +1,13 @@
+x =input("Enter number one: ")
+x =int(x)
+y =input("Enter number two: ")
+y =int(y)
+sksk =input("Addition or Subtraction? ")
+if sksk == ("addition"):
+    print(x+y)
+elif sksk == ("Addition"):
+    print(x+y)
+elif sksk == ("subtraction"):
+    print(x-y)
+elif sksk == ("Subtraction"):
+    print(x-y)
