cfd359f0305e38c2ee50c024260b5a33c8394dac
jeanniechamp <jeanniechamp@gmail.com>
2023-11-03 12:25:06 +0530

    DAY 3 AND 4

--- exercise/7voting_age.py
+++ exercise/7voting_age.py
@@ -1,6 +1,6 @@
 x =input("What is your age? ")
 x =int(x)
-if x > 18:
+if x >= 18:
     print("You are of legal voting age!")
-elif x < 18:
+else:
     print("Sorry, you can't vote because you are under 18!")
---
+++ exercise/8senior_age.py
@@ -0,0 +1,6 @@
+x = input("What is your age? ")
+x =int(x)
+if x >= 60:
+    print("You are a seniour citizen.")
+else:
+    print("You are not a seniour citizen yet.")
