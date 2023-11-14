475b5b9b473e199aa6c5fbdac4225415b9fb0014
jeanniechamp <jeanniechamp@gmail.com>
2023-11-14 22:33:53 +0530

    hhh

---
+++ exercise/10largest_no.py
@@ -0,0 +1,10 @@
+##Exercise: Write a program to find the largest number out of two numbers excepted from user.
+x =input("Enter number 1: ")
+y =input("Enter no. 2: ")
+y=int(y)
+x=int(x)
+if x > y:
+    print( x,"is greater than",y)
+else:
+    print(y,"is greater than",x)
+    
