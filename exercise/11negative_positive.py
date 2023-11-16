c839ca91a9e393ff160be97ebba16d9e897eab66
jeanniechamp <jeanniechamp@gmail.com>
2023-11-16 11:45:18 +0530

    Debugging done

--- exercise/11negative_positive.py
+++ exercise/11negative_positive.py
@@ -1,7 +1,7 @@
 ##Exercise: Write a program to check whether a number (accepted from user) is positive or negative.
 x = input("Enter a number: ")
 x=int(x)
-if -x:
+if x < 0:
     print("This number is negative")
 elif x:
     print("This number is positive")
