323926baa738aba9b3fc1fad2c5e5a1b38a7a257
jeanniechamp <jeanniechamp@gmail.com>
2023-11-17 13:31:35 +0530

    done

---
+++ exercise/12even_odd.py
@@ -0,0 +1,7 @@
+##Exercise: Write a program to check whether a number is even or odd.
+x=input("Enter a number: ")
+x=int(x)
+if x%2 == 0:
+    print("this number is even")
+else:
+    print("this number is odd")
