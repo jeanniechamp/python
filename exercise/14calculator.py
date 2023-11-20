342b48b6fde4a07dc8dc911f8826701723698fd3
jeanniechamp <jeanniechamp@gmail.com>
2023-11-20 23:24:48 +0530

    HELP !

---
+++ exercise/14calculator.py
@@ -0,0 +1,14 @@
+#Exercise: Calculator Create a script that accepts 2 numbers and an operator (+, -, *, /), and prints the result of the operation
+x =input("Enter number 1: ")
+y=input("Enter number 2: ")
+o =input("Which operation? (+ , -, *, / ) ")
+x=int(x)
+y=int(y)
+if o ("+"):
+    print (x+y)
+elif o ("-"):
+    print (x-y)
+elif o ("*"):
+    print (x*y)
+elif o ("/"):
+    print (x/y)
