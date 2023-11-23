3734f89ae1f753f4b332ffa3c4508dc18b9cb4a2
jeanniechamp <jeanniechamp@gmail.com>
2023-11-23 23:05:30 +0530

    ver2

--- exercise/15numguess_v1.py
+++ exercise/15numguess_v1.py
@@ -1,12 +1,12 @@
 import random
 
 n = random.randint(1, 20)
-guess = input("Guess the no. between 1- 20: ")
+guess = input("Guess the secret number between 1- 20: ")
 guess =int(guess)
 if guess == n:
     print("Your guess is correct!")
 elif guess > n:
-    print("Your guess is less than the secret no. ")
+    print("Your guess is", guess - n ,"less")
 elif guess < n:
-    print("Your guess is greater than the secret no.")
+    print("Your guess is", n - guess ,"greater")
 print ("the number was",n)
